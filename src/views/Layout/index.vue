abf72266dc1f87d36836534801196307abbca414
weibaoan <3255427824@qq.com>
2024-03-20 19:45:00 +0800

    第1模块-项目初始化

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>我是首页的页面布局</h1>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>我是登录页</h1>
+</template>
+
+<style scoped></style>
