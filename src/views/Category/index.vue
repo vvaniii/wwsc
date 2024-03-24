8fb0e1e4269f8367ea44de07cffe458a57cb725f
weibaoan <3255427824@qq.com>
2024-03-24 14:31:14 +0800

    一级分类-轮播图

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,13 +2,20 @@
 const categoryData = ref({});
 const route = useRoute();
 const getCategory = async () => {
-  // 如何在setup中获取路由参数 useRoute() -> route 等价于this.$route
-  console.log(route.params.id);
   const res = await getTopCategoryAPI(route.params.id);
 
   categoryData.value = res.result;
 };
 onMounted(() => getCategory());
+
+//轮播图
+const bannerList = ref([]);
+
+const getBanner = async () => {
+  const res = await getBannerAPI("2");
+  bannerList.value = res.result;
+};
+onMounted(() => getBanner());
 </script>
 
 <template>
@@ -21,6 +28,30 @@ onMounted(() => getCategory());
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
